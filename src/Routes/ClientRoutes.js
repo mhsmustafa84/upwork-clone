1688e1d51a4e7bad0cf0413feaa61c2649ab1464
alaa-refat <alaarefatmohmmed@gmail.com>
2021-04-13 13:48:42 +0200

    edit talent page component creation

--- src/Routes/ClientRoutes.js
+++ src/Routes/ClientRoutes.js
@@ -6,12 +6,14 @@ import {
     Link
   } from "react-router-dom";
 
+
 export default function ClientRoutes() {
     return (
         <>
         <Router>
             <Switch>
                 <Route/>
+                {/* <Route path="/saved" component={Saved}/> */}
             </Switch>
         </Router>
         </>
