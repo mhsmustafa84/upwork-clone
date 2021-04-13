3722e27c1f90a505639c2befd4a011ae3ecd5b82
alaa-refat <alaarefatmohmmed@gmail.com>
2021-04-13 15:50:10 +0200

    edit talent tabs pathes in client

--- src/Pages/ClientPages/Talent/Talent.js
+++ src/Pages/ClientPages/Talent/Talent.js
@@ -1,24 +1,35 @@
 import React from 'react'
+import { Link, Route, BrowserRouter as Router, Switch } from 'react-router-dom'
+import MyHires from "../../../Components/ClientComponents/MyHiresComponent/MyHires"
+import Saved from "../../../Components/ClientComponents/SavedComponent/Saved"
+
 
 export default function Talent() {
     return (
         <div>
-            {/* <div class="container">
-                <ul class="nav nav-tabs">
-                    <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="#">Active</a>
+            <div className="container">
+            <Router>
+                <ul className="nav nav-tabs">
+                    <li className="nav-item">
+                        <Link className="nav-link active" to="/ab/profiles/search/" >Search</Link>
+                        {/* <a className="nav-link active" aria-current="page" href="#">Active</a> */}
                     </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Link</a>
+                    <li className="nav-item">
+                        <Link className="nav-link" to="/ab/profiles/search/hires" >My Hires</Link>
+                        {/* <a className="nav-link" href="#">Link</a> */}
                     </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Link</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+                    <li className="nav-item">
+                        <Link className="nav-link" to="/ab/profiles/search/saved" >Saved</Link>
+                        {/* <a className="nav-link" href="#">Link</a> */}
                     </li>
                 </ul>
-            </div> */}
+           
+                <Switch>
+                    <Route path="/ab/profiles/search/hires" exact component={MyHires} />
+                    <Route path="/ab/profiles/search/saved" exact component={Saved} />
+                </Switch>
+            </Router>
+            </div>
         </div>
     )
 }
--- src/Routes/ClientRoutes.js
+++ src/Routes/ClientRoutes.js
@@ -5,17 +5,12 @@ import {
     Route,
     Link
   } from "react-router-dom";
-import Saved from "../Components/ClientComponents/SavedComponent/Saved"
-import MyHires from "../Components/ClientComponents/MyHiresComponent/MyHires"
-
 export default function ClientRoutes() {
     return (
         <>
         <Router>
             <Switch>
-                <Route/>
-                <Route path="/ab/profiles/search/hires" exact component={MyHires}/>
-                <Route path="/ab/profiles/search/saved" exact component={Saved}/>
+                <Route />
             </Switch>
         </Router>
         </>
