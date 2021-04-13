fd71a57445f18a7b072bd0f752ea3deb5ab47bfc
alaa-refat <alaarefatmohmmed@gmail.com>
2021-04-13 12:54:49 +0200

    testing talent page in app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Client from './LayOut/Client';
+import Talent from './Pages/ClientPages/Talent/Talent';
 
 function App() {
   return (
     <div className="App">
-      <Client/>
+      {/* <Client/> */}
+      <Talent />
     </div>
   );
 }
---
+++ src/Pages/ClientPages/Talent/Talent.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+export default function Talent() {
+    return (
+        <div>
+            {/* <div class="container">
+                <ul class="nav nav-tabs">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">Active</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Link</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Link</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+                    </li>
+                </ul>
+            </div> */}
+        </div>
+    )
+}
