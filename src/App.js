95bd144422650ac7221d92fcb9d869bcb025b78a
alaa-refat <alaarefatmohmmed@gmail.com>
2021-04-15 10:47:52 +0200

    alaa branch

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import './App.css';
 import Client from './LayOut/Client';
-import FindFreelancingJob from './Pages/TalentPages/FindFreelancingJob/FindFreelancingJob';
 
 function App() {
   return (
     <div className="App">
-      {/* <Client/> */}
-      <FindFreelancingJob />
+      <Client/>
     </div>
   );
 }
