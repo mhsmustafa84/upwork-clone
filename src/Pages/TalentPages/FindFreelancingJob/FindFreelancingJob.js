805a963f82eff0f7c0fed5f8bef6acfc366aa13c
alaa-refat <alaarefatmohmmed@gmail.com>
2021-04-15 06:09:00 +0200

    creation of type of work section for find Freelancing job page in talent pages

--- src/Pages/TalentPages/FindFreelancingJob/FindFreelancingJob.js
+++ src/Pages/TalentPages/FindFreelancingJob/FindFreelancingJob.js
@@ -1,7 +1,9 @@
 import React from 'react'
+import './FindFreelancingJob.css'
 import FindJobsHeader from '../../../Components/TalentComponents/FindJobsHeader/FindJobsHeader'
 import ListOfWork from '../../../Components/TalentComponents/ListOfWork/ListOfWork'
 import TypeOfWork from '../../../Components/TalentComponents/TypeOfWork/TypeOfWork'
+import TypeOfWorkSmallSize from '../../../Components/TalentComponents/TypeOfWorkSmallSize/TypeOfWorkSmallSize'
 
 export default function FindFreelancingJob() {
     return (
@@ -9,7 +11,12 @@ export default function FindFreelancingJob() {
             <FindJobsHeader />
             <section class="container py-3 px-5 small" id="main-section-ID">
                 <div class="row ">
+                <div className="col-xl-3 px-5 d-none d-xl-block">
                     <TypeOfWork />
+                </div>
+                    <div className="row d-block d-xl-none px-lg-5 px-1 py-3">
+                        <TypeOfWorkSmallSize />
+                    </div>
                     <div class="col-xl-9 col-12 px-lg-5 px-1 py-3">
                         <ListOfWork character="A" />
                         <ListOfWork character="B" />
