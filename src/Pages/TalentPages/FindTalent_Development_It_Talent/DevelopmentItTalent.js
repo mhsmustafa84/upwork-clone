053efbff70ce513b1d9f8fa5dc08bdc698d42039
alaa-refat <alaarefatmohmmed@gmail.com>
2021-04-15 03:33:56 +0200

    creation of questions article for talent in talent pages

--- src/Pages/TalentPages/FindTalent_Development_It_Talent/DevelopmentItTalent.js
+++ src/Pages/TalentPages/FindTalent_Development_It_Talent/DevelopmentItTalent.js
@@ -14,6 +14,7 @@ import GetStarted from '../../../Components/TalentComponents/GetStarted/GetStart
 import PlayingField from '../../../Components/TalentComponents/PlayingField/PlayingField'
 import ProjectExamples from '../../../Components/TalentComponents/ProjectExamples/ProjectExamples'
 import Flexera from '../../../Components/TalentComponents/Flexera/Flexera'
+import Questions from '../../../Components/TalentComponents/Questions/Questions'
 
 
 export default function DevelopmentItTalent() {
@@ -45,6 +46,7 @@ export default function DevelopmentItTalent() {
                     <PlayingField />
                     <ProjectExamples />
                     <Flexera />
+                    <Questions />
             </section>
 
         </div>
