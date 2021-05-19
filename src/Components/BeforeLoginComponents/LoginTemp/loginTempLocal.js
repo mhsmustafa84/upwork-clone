509a35cb4aa19a030656651a93efce00462bac9b
Romany-remo <33420600+Romany-remo@users.noreply.github.com>
2021-05-19 22:33:01 +0200

    after pull by romany

--- src/Components/BeforeLoginComponents/LoginTemp/loginTempLocal.js
+++ src/Components/BeforeLoginComponents/LoginTemp/loginTempLocal.js
@@ -1,16 +1,25 @@
 const lang = {
-    ar: {
-        loginandget: "تسجيل الدخول والحصول على وظيفة",
-        keepme: "البقاء تسجيل الدخول ",
-        currentWeek: "الأسبوع الحالي",
-        WeeklySummaryByClient: "ملخص الأسبوع حسب العميل"
+    ar : {
+        Loginandgettowork: "سجل الدخول وابدأ العمل",
+        Keepmeloggedin:"تذكرني",
+        Forgotpassword:"هل نسيت كلمة السر؟",
+        Login:"تسجيل الدخول",
+        Notyou:"ليس انت؟",
+        Signinwithgoogle:" تسجيل الدخول مع جوجل",
+        signinwithapple:"تسجيل الدخول مع ابل",
+        NewToUpwork:"جديد في Upwork؟",
+        SignUp:"اشتراك"
     },
-    en: {
-        loginandget: " Log in and get to work",
-        keepme: " Keep me logged in",
-        currentWeek: "Current week",
-        WeeklySummaryByClient: "Weekly summary by client"
+    en : {
+        Loginandgettowork: "Log in and get to work",
+        Keepmeloggedin:"Keep me logged in",
+        Forgotpassword:"Forgot password?",
+        Login:"Login",
+        Notyou:"Not you?",
+        Signinwithgoogle:"Sign in with google",
+        signinwithapple:"sign in with apple",
+        NewToUpwork:"New to Upwork?",
+        SignUp:" Sign Up"
     }
 };
-
 export default lang;
