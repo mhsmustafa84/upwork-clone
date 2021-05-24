07cf2645a146eafb19d7cdf3f2368de0e38b5122
Romany-remo <33420600+Romany-remo@users.noreply.github.com>
2021-05-24 17:19:22 +0200

    routing proposal page, add proposal page link in navbar talent, ,,, and localization of all of them + sign up + signin add spaces in keys

--- src/Components/TalentComponents/ProposalCard/ProposalCard.js
+++ src/Components/TalentComponents/ProposalCard/ProposalCard.js
@@ -1,23 +1,26 @@
 import React from 'react'
+import { useTranslation } from "react-i18next";
+
 
 export default function ProposalCard() {
+    const { t } = useTranslation();
     return (
         <div>
             <div className="row">
                         <div className="col">
                             <div>
-                                Initiated Apr 27, 2021
+                                {t("Initiated")} Apr 27, 2021
 
                             </div>
                             <div className="text-muted">
-                                a month ago
+                                {t("a month ago")}
                             </div>
                         </div>
                         <a href="#" className="col-6 fw-bold text-success ">
                             Create 3D Mock Up File for Phone Case Packaging
                         </a>
                         <div className="col text muted">
-                            Graphic Design
+                            {t("Graphic Design")}
                         </div>
                     </div>
                     <hr/>
