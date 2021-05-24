89f99b860618271fb8c122fb535281237499b619
AhmedGamalSaiid <74775097+AhmedGamalSaiid@users.noreply.github.com>
2021-05-24 16:21:14 +0200

    handle unuse val

--- src/Components/TalentComponents/SearchBarJobsTalent/SearchBarJobsTalent.js
+++ src/Components/TalentComponents/SearchBarJobsTalent/SearchBarJobsTalent.js
@@ -1,4 +1,4 @@
-import React,{ useContext, useEffect, useState } from "react";
+import React,{ useContext, useEffect } from "react";
 import { db } from "../../../firebase";
 import { Link, useHistory } from "react-router-dom";
 import { useTranslation } from "react-i18next";  
