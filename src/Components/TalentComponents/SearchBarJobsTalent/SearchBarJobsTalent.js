51c2dcd8198b4093d99e3f69e318e405748018fa
alaa-refat <alaa.refat1996@gmail.com>
2021-05-24 16:09:14 +0200

    search input functionality in talent pages for jobs

--- src/Components/TalentComponents/SearchBarJobsTalent/SearchBarJobsTalent.js
+++ src/Components/TalentComponents/SearchBarJobsTalent/SearchBarJobsTalent.js
@@ -1,61 +1,75 @@
-import React,{ useState } from "react";
-import firebaseApp from "../../../firebase";
-import { Link } from "react-router-dom";
+import React,{ useContext, useEffect } from "react";
+import { db } from "../../../firebase";
+import { Link, useHistory } from "react-router-dom";
 import { useTranslation } from "react-i18next";  
+import { SearchContext } from '../../../Context/SearchContext';
+import { updateUserData } from '../../../Network/Network'
+import { useDispatch, useSelector } from "react-redux";
+import { talentDataAction } from "../../../Store/actions/talentData";
 
 
-
-export default function SearchBarJobsTalent() {
+export default function SearchBarJobsTalent(props) {
   const { t }=useTranslation();
-    const [verify, setverify] = useState(false);
-  firebaseApp.auth().onAuthStateChanged((user) => {
-    if (user) {
-      //console.log(user.emailVerified);
-      var verf = user.emailVerified;
-      setverify(verf);
-    }
-  });
+  const { push } = useHistory();
+  const { arr, setarr, itemSearchList, setitemSearchList } = useContext(SearchContext)
+  const user = useSelector((state) => state.talentData);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(talentDataAction());
+    console.log(user);
+  }, []);
 
-    return (
-        <div>
-          {!verify && (
-            <div
-              class="alert alert-warning alert-dismissible fade show"
-              role="alert"
-            >
-              <strong>{t("Email Verification")}</strong> {t("Your mail is not verified")}
-              <button
-                type="button"
-                class="btn-close"
-                data-bs-dismiss="alert"
-                aria-label="Close"
-              ></button>
-            </div>
-          )}
-          <div className="col-8 input-group form-outline has-success">
-            <input
-              id="input"
-              type="search"
-              className="form-control text-dark bg-white btn-outline-success"
-              placeholder={t("Search For Jobs")}
-            />
-             <Link to="/Search">
-            <button
-              id="search-button"
-              type="button"
-              className="btn bg-upwork bg-invert"
+  const handle = (e) => {
+    setitemSearchList(e.target.value)
+  }
 
-            >
-              <i className="fas fa-search" />
-            </button>
-            </Link>
-          </div>
-          <span>
-            <a href="#" className="advanced-search-link">
-              {t("AdvancedSearch")}
-            </a>
-          </span>
-        </div>
-        
-    )
+  const searchDatabase = () => {
+    let tempArr = [];
+    db.collection('job')
+    .where('skills', 'array-contains', itemSearchList)
+    .onSnapshot(
+      jobs=>jobs.docs.map(
+        item=>{
+        tempArr.push(item.data())
+        push({pathname:"/search",state:tempArr})
+      }))
+
+      if (itemSearchList != "") {
+       let  arr2 = [itemSearchList,...arr];       
+        updateUserData('talent', { searchHistory: [...user?.searchHistory,...arr2] })
+        setarr(arr2);
+        sessionStorage.setItem('searchArray',JSON.stringify(arr2))
+      }
+}
+
+  return (
+    <div>
+      <div className="col-8 input-group form-outline has-success">
+        <input
+          onChange={handle}
+          value={itemSearchList}
+          id="input"
+          type="search"
+          className="form-control text-dark bg-white btn-outline-success"
+          placeholder={t("Search For Jobs")}
+        />
+        <Link onClick={searchDatabase}>
+          <button
+            id="search-button"
+            type="button"
+            className="btn bg-upwork bg-invert search"
+          >
+
+            <i className="fas fa-search" />
+          </button>
+        </Link>
+      </div>
+      <span>
+        <a href="#" className="advanced-search-link">
+          {t("AdvancedSearch")}
+        </a>
+      </span>
+    </div>
+
+  )
 }
