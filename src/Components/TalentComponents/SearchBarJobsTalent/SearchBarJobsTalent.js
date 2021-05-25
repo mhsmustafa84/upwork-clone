b75f3cba46afd725a1135d18262dd380b149fcb6
AhmedGamalSaiid <74775097+AhmedGamalSaiid@users.noreply.github.com>
2021-05-25 13:09:04 +0200

    search field from db

--- src/Components/TalentComponents/SearchBarJobsTalent/SearchBarJobsTalent.js
+++ src/Components/TalentComponents/SearchBarJobsTalent/SearchBarJobsTalent.js
@@ -42,8 +42,11 @@ export default function SearchBarJobsTalent(props) {
       if (itemSearchList !="") {
         let  arr2=[]
         arr != null ?arr2 = [itemSearchList,...arr] : 
-        arr2=[itemSearchList]        
+        arr2=[itemSearchList]
+        user.searchHistory !=null ?   
         updateUserData('talent', { searchHistory: [...user?.searchHistory,...arr2] })
+        :updateUserData('talent', { searchHistory: [...arr2] })
+
         sessionStorage.setItem('searchArray',JSON.stringify(arr2))
         setarr([...arr2])
       }
