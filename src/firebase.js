91f4e12ace3d54798d0a6ecbadd38148caa87373
AhmedGamalSaiid <74775097+AhmedGamalSaiid@users.noreply.github.com>
2021-06-06 11:04:12 +0200

    fix firebase

--- src/firebase.js
+++ src/firebase.js
@@ -9,7 +9,7 @@ const firebaseApp = firebase.initializeApp({
   projectId: "final-project-react-12190",
   storageBucket: "final-project-react-12190.appspot.com",
   messagingSenderId: "1063777536149",
-  appId: "1:1063777536149:web:ac906c833922665c00739c",
+  appId: "1:1063777536149:web:ac906c833922665c00739c", 
   measurementId: "G-8BVMCH1W3X",
 });
 // const firebaseApp = firebase.initializeApp({
@@ -32,7 +32,7 @@ const firebaseApp = firebase.initializeApp({
 // });
 
 const auth = firebaseApp.auth();
-const db = firebaseApp.firestore();
+const db = firebaseApp.firestore(); 
 const storage = firebaseApp.storage();
 const googleProvider = new firebase.auth.GoogleAuthProvider();
 const appleProvider = new firebase.auth.OAuthProvider("apple.com");
