6053f0059440804d1cb595d9e6a235944d001fda
Mostafa AbdElHamid <mhsmustafa84@gmail.com>
2021-06-10 13:13:42 +0200

    test

--- src/Pages/BeforeLoginPages/HomePage/HomePage.js
+++ src/Pages/BeforeLoginPages/HomePage/HomePage.js
@@ -5,15 +5,10 @@ import HomeThirdSection from './../../../Components/BeforeLoginComponents/HomeTh
 import HomeFourthSection from './../../../Components/BeforeLoginComponents/HomeFourthSection/HomeFourthSection';
 import Header from '../../../Components/BeforeLoginComponents/Header/Header';
 import Footer from '../../../Components/BeforeLoginComponents/Footer/Footer';
-import { db } from '../../../firebase';
 
 export default function HomePage() {
-    const add = () => {
-        db.collection("mostafa").add({ name: "mostafa" }).then(res => console.log("add")).catch(err => console.log(err));
-    }
     return (
         <div className="bg-white">
-            <button onClick={add}>click</button>
             <Header />
             <HomeFirstSection />
             <HomeSecondSection />
