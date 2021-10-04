e22a15e7d90d37945862b4aeef9a5af0d84f5e62
AhmedGamalSaiid <74775097+AhmedGamalSaiid@users.noreply.github.com>
2021-10-04 10:30:30 +0200

    translation

--- src/App.js
+++ src/App.js
@@ -4,15 +4,15 @@ import LayOut from "./LayOut/LayOut";
 import { useSelector } from "react-redux";
 import "./App.css";
 
-
 function App() {
-  let lang = useSelector(state => state.lang)
+  let lang = useSelector((state) => state.lang);
   return (
     <BrowserRouter>
       <div
         dir={lang === "ar" ? "rtl" : "ltr"}
         lang={lang === "ar" ? "ar" : "en"}
-        style={{ fontFamily: "'Cairo', sans-serif !important" }}>
+        style={{ fontFamily: "'Cairo', sans-serif !important" }}
+      >
         <LayOut />
       </div>
     </BrowserRouter>
