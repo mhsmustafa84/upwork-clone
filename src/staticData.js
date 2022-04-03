d43802f5b07ed3ef2441aad8bfb65bfe72fa8c96
Mostafa AbdElHamid <mhsmustafa84@gmail.com>
2022-04-03 21:02:40 +0200

    initiate static data file

---
+++ src/staticData.js
@@ -0,0 +1,30 @@
+export const staticData = {
+    beforeLogin: {
+        login: {
+            ar: "تسجيل الدخول",
+            en: "Log In"
+        },
+        signUp: {
+            ar: "انشاء حساب",
+            en: "Sign Up"
+        },
+        alreadyHaveAccount: {
+            ar: "",
+            en: "Already Have an Account?"
+        },
+        continueWithEmail: {
+            ar: "",
+            en: "Continue with Email"
+        },
+        emailRequired: {
+            ar: "",
+            en: "Email Required"
+        }
+    },
+    talent: {
+
+    },
+    client: {
+
+    }
+}
