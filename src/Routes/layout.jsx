7a1bc575f63d1d893eaa87d9ea6c6bdc6551fa22
Mostafa AbdElHamid <mhsmustafa84@gmail.com>
2022-04-08 23:57:50 +0200

    fix laout file, user type

--- src/Routes/layout.jsx
+++ src/Routes/layout.jsx
@@ -8,29 +8,22 @@ let usrType = null;
 
 export const Layout = () => {
 
-    const [usr, setUsr] = useState(null);
+    const [user, setUser] = useState(null);
 
     useEffect(() => {
         fbAuth.onAuthStateChanged(fbAuth.auth, user => {
             if (user) {
                 console.log('file: layout.jsx => line 15 => useEffect => user', user);
-                setUsr(user);
+                setUser(user);
                 usrType = localStorage.getItem('userType') || null;
             }
         });
     }, []);
 
-    if (usr) {
-        if (usrType === "talent") {
-            return <Talent />
-        }
-        if (usrType === "client") {
-            return <Client />
-        }
-        if (usrType !== null) {
-            return <Loader />
-        }
-    } else {
-        return <UnAuth />;
+    if (user) {
+        if (usrType === "talent") return <Talent />;
+        if (usrType === "client") return <Client />;
+        if (usrType !== null) return <Loader />;
     }
+    if (!user) return <UnAuth />;
 }
