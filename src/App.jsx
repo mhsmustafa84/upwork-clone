83f4d240ce8dbb07d01db9b6fb8ed28c30f89a0f
Mostafa AbdElHamid <mhsmustafa84@gmail.com>
2022-04-09 00:00:13 +0200

    fix app file

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,8 @@ import { BrowserRouter } from "react-router-dom";
 import { Layout } from "./Routes/layout";
 import "./App.css";
 
-
 function App() {
-    console.log(process.env.REACT_APP_BASE_ROUTE);
+    console.log('file: App.jsx => App => REACT_APP_BASE_ROUTE', process.env.REACT_APP_BASE_ROUTE);
     return (
         <BrowserRouter>
             <Layout />
