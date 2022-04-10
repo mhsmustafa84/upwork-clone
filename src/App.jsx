bfd5861c625066dc0e22af5e585099337ea7ce61
Mostafa AbdElHamid <mhsmustafa84@gmail.com>
2022-04-11 01:37:49 +0200

    format fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import { BrowserRouter } from "react-router-dom";
-import { Layout } from "./routes/layout";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import "./App.css";
+import { BrowserRouter } from 'react-router-dom'
+import { Layout } from './routes/layout'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './App.css'
 
 export const App = () => {
     return (
         <BrowserRouter>
             <Layout />
         </BrowserRouter>
-    );
+    )
 }
