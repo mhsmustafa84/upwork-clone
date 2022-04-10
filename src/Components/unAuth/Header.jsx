111cbca7d282781b6bb291191e040fb27692a9b1
Mostafa AbdElHamid <mhsmustafa84@gmail.com>
2022-04-11 00:17:37 +0200

    fix home header

--- src/Components/unAuth/Header.jsx
+++ src/Components/unAuth/Header.jsx
@@ -9,12 +9,8 @@ export const Header = () => {
         <header className="fixed-top bg-white">
             <div className="container border-bottom py-3">
                 <div className="d-flex justify-content-between align-items-center">
-                    <div className="d-flex ">
-                        <div className="d-flex me-5">
-                            <Logo url={process.env.PUBLIC_URL} />
-                        </div>
-                    </div>
-                    <div className="col-md-5 ps-2 d-flex j justify-content-end">
+                    <Logo url={process.env.PUBLIC_URL} />
+                    <div>
                         <Link
                             className="btn text-main-color me-2"
                             to={`${process.env.PUBLIC_URL}/login`}
@@ -22,7 +18,7 @@ export const Header = () => {
                             Log In
                         </Link>
                         <Link
-                            className="btn upw-bg-color text-white rounded-pill px-4 py-2 fs-6"
+                            className="btn upw-bg-color text-white rounded-pill px-5 fs-7"
                             to={`${process.env.PUBLIC_URL}/sign-up`}
                         >
                             Sign Up
