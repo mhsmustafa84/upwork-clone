bb67d6a2d5595c12ace42d4525560e9f5e7f97ae
Mostafa AbdElHamid <mhsmustafa84@gmail.com>
2022-04-10 20:45:37 +0200

    remove signup details page

--- src/Pages/unAuth/SignUp.jsx
+++ src/Pages/unAuth/SignUp.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import LoginHeader from "../../components/unAuth/LoginHeader";
 import { SignupForm } from '../../components/unAuth/SignupForm';
 import { Footer } from '../../components/shared/footer/Footer';
+import SignUpSecondTemp from '../../components/unAuth/SignUp Second Temp/SignUpSecondTemp';
 
 export const SignUp = () => {
     return (
         <>
             <LoginHeader />
             <SignupForm />
+            <SignUpSecondTemp />
             <Footer />
         </>
     );
