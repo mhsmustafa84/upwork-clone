80bddf11fb4ca99561b9f16e468b02a4cb219ac4
Mostafa Abdelhamid <mhsmustafa84@gmail.com>
2026-08-01 23:46:38 +0300

    Refactor firebase config to env variables and update redux devtools extension

--- src/Store/store.js
+++ src/Store/store.js
@@ -1,5 +1,5 @@
 import { createStore, applyMiddleware } from "redux";
-import { composeWithDevTools } from "redux-devtools-extension";
+import { composeWithDevTools } from "@redux-devtools/extension";
 import reducers from "./combineReducers";
 import thunk from "redux-thunk";
 
--- src/firebase.js
+++ src/firebase.js
@@ -4,13 +4,13 @@ import "firebase/storage";
 import "firebase/firestore";
 
 const firebaseApp = firebase.initializeApp({
-  apiKey: "AIzaSyBYCGYP-tx6DJKgjd4cNPiYoOC3wNk1cwk",
-  authDomain: "final-project-react-12190.firebaseapp.com",
-  projectId: "final-project-react-12190",
-  storageBucket: "final-project-react-12190.appspot.com",
-  messagingSenderId: "1063777536149",
-  appId: "1:1063777536149:web:ac906c833922665c00739c", 
-  measurementId: "G-8BVMCH1W3X",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID, 
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 });
 
 
